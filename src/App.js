7300c4c5f2730715ffb935af48554196e47c92f1
Amit Saini <amitssaini999@gmail.com>
2024-06-09 12:47:50 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetch("https://restcountries.com/v3.1/all");
+        const data = await resp.json();
+        setCountries(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const data = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
+    setFiltered(data);
+  }, [search]);
+
+  console.log(countries);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="inp">
+        <input
+          type="text"
+          placeholder="Enter a country"
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div className="App">
+        {search === ""
+          ? countries.map((country) => {
+              return (
+                <div className="countryCard">
+                  <img src={country.flags.png} alt={country.flag}></img>
+                  <p>{country.name.common}</p>
+                </div>
+              );
+            })
+          : filtered.map((country) => {
+              return (
+                <div className="countryCard">
+                  <img src={country.flags.png} alt={country.flag}></img>
+                  <p>{country.name.common}</p>
+                </div>
+              );
+            })}
+      </div>
     </div>
   );
 }
